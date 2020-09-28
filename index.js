c646286e6c20859848f416e635f66f9e134e95fc
looping-torrent-8105 <gianherrera96@gmail.com>
2020-09-28 17:44:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object,key){
-  var copy = Object.assign({}, object, {[key]: value})
-  return copy;
+ delete object.key;
+ return object
 }
